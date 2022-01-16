9b2447b055e62337a2521c69288b580a1a333422
smacdonald97 <smacdonald97@email.arizona.edu>
2022-01-16 14:26:57 -0700

    created switch for password conditions

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,48 @@
 // Assignment code here
+//Adding the constants for password generation
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "1234567890";
+const symbols = "!@#$%^&*_-+=";
+
+
+
+
 var generatePassword = function(){
+    //length variable for security purposes
+    var length = 8;
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        charset = "!@#$%^&*-_+="
+        //stack overflow taught me about the return value!
+        retval = " "
+    for (var i= 0, n=charset.length; i<length; ++i)
+        return retval;
+    //creating a conditionPrompt that can read integer input
     conditionPrompt= parseInt(conditionPrompt);
-    var conditionPrompt = window.prompt("What conditions would you like for your secure password? Please enter one: 1.Capital Letters, 2. Special Characters, 3. 8 or more characters, 4. Capital letters and special characters, 5. Eight or more characters and special characters, 6. Eight or more characters and capital letters, 7. All conditions")
-    //creating a switch dependent on user input
+    var conditionPrompt = window.prompt("What conditions would you like for your secure password? Please enter one: 1.Capital Letters, 2. Special Characters, 3. Capital letters and special characters, 4. All conditions")
+    //creating a switch dependent on user input for password conditions
     switch (conditionPrompt){
         case 1: 
+            length
+            charset= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
             break;
         case 2:
+            length
+            charset= "!@#$%^&*-_+="
             break;
         case 3:
+            length
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*-_+="
             break;
         case 4:
-            break;
-        case 5: 
-            break;
-        case 6: 
-            break;
-        case 7:
+            length
+            charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*-_+="
             break;
         default:
-            window.alert("You did not pick a valid response. Please enter 1 for capital letters, 2 for special characters, 3 for 8 or more characters, 4 for capital letters and special characters, 5 for special characters and eight or more characters")
+            window.alert("You did not pick a valid response. Please enter 1 for capital letters, 2 for special characters, 3 for capital letters and special characters, 4 for all conditions")
     }
 }
 
+generatePassword();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
